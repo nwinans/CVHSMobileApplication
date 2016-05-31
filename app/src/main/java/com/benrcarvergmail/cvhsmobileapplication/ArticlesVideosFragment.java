b7a1d2aaac9f38fe63976e24776fac30440fad81
kocopuff9191 <kocopuff9191@gmail.com>
2016-05-31 11:09:02 -0400

    Korean and article video

--- app/src/main/java/com/benrcarvergmail/cvhsmobileapplication/ArticlesVideosFragment.java
+++ app/src/main/java/com/benrcarvergmail/cvhsmobileapplication/ArticlesVideosFragment.java
@@ -4,22 +4,24 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
- * Created by Benjamin on 5/20/2016.
+ * Created by Yoohan on 5/31/2016.
  */
 public class ArticlesVideosFragment extends Fragment {
 
     private final static String TAG = "ArticlesVideosFragment";
 
     private LinearLayout mLinearLayoutArticles;
-    private LinearLayout mLinearLayoutVideos;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,58 +30,27 @@ public class ArticlesVideosFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_articlesvideos, container, false);
 
-        final Button buttonArticles = (Button) rootView.findViewById(R.id.button_articles);
-        final Button buttonVideos = (Button) rootView.findViewById(R.id.button_videos);
-
         mLinearLayoutArticles = (LinearLayout) rootView.findViewById(R.id.linear_layout_articles);
-        mLinearLayoutVideos = (LinearLayout) rootView.findViewById(R.id.linear_layout_videos);
 
-        // Since 'Articles' is selected by default, set the
-        // articles button's text color to be the lighter green
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            buttonArticles.setTextColor(getResources().getColor(R.color.colorButtonLightGreen, null));
-            buttonVideos.setTextColor(getResources().getColor(R.color.colorButtonDarkGreen, null));
-        } else {
-            buttonArticles.setTextColor(getResources().getColor(R.color.colorButtonLightGreen));
-            buttonVideos.setTextColor(getResources().getColor(R.color.colorButtonDarkGreen));
-        }
+        String text1 = "<a href='http://www.nationaleatingdisorders.org/sites/default/files/Toolkits/EducatorToolkit.pdf'> Educator Toolkit </a>";
+        String text2 = "<a href='http://www.nationaleatingdisorders.org/general-statistics'> General Statistics </a>";
+        String text3 = "<a href='http://www.nationaleatingdisorders.org/what-should-i-say'> What should I say? </a>";
+        String text4 = "<a href='http://www.nationaleatingdisorders.org/what-body-image'> Body Image </a>";
 
-        buttonArticles.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mLinearLayoutArticles.setVisibility(View.VISIBLE);
-                mLinearLayoutVideos.setVisibility(View.GONE);
+        TextView link1 = (TextView) rootView.findViewById(R.id.educator_toolkit);
+        TextView link2 = (TextView) rootView.findViewById(R.id.general_statistics);
+        TextView link3 = (TextView) rootView.findViewById(R.id.what_should_i_say);
+        TextView link4 = (TextView) rootView.findViewById(R.id.what_body_image);
 
-                // Ensure we use non-deprecated code to change the text color
-                // to indicate the selected type of content (articles vs videos)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    buttonArticles.setTextColor(getResources().getColor(R.color.colorButtonLightGreen, null));
-                    buttonVideos.setTextColor(getResources().getColor(R.color.colorButtonDarkGreen, null));
-                } else {
-                    buttonArticles.setTextColor(getResources().getColor(R.color.colorButtonLightGreen));
-                    buttonVideos.setTextColor(getResources().getColor(R.color.colorButtonDarkGreen));
-                }
-            }
-        });
+        link1.setMovementMethod(LinkMovementMethod.getInstance());
+        link2.setMovementMethod(LinkMovementMethod.getInstance());
+        link3.setMovementMethod(LinkMovementMethod.getInstance());
+        link4.setMovementMethod(LinkMovementMethod.getInstance());
 
-        buttonVideos.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                mLinearLayoutArticles.setVisibility(View.GONE);
-                mLinearLayoutVideos.setVisibility(View.VISIBLE);
-
-                // Ensure we use non-deprecated code to change the text color
-                // to indicate the selected type of content (articles vs videos)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    buttonArticles.setTextColor(getResources().getColor(R.color.colorButtonDarkGreen, null));
-                    buttonVideos.setTextColor(getResources().getColor(R.color.colorButtonLightGreen, null));
-                } else {
-                    buttonArticles.setTextColor(getResources().getColor(R.color.colorButtonDarkGreen));
-                    buttonVideos.setTextColor(getResources().getColor(R.color.colorButtonLightGreen));
-                }
-            }
-        });
+        link1.setText(Html.fromHtml(text1));
+        link2.setText(Html.fromHtml(text2));
+        link3.setText(Html.fromHtml(text3));
+        link4.setText(Html.fromHtml(text4));
 
         return rootView;
     }
